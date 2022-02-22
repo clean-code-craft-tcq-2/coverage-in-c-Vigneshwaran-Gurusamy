b7cb523a93834adcadd6ad6d7af8c7b4f7c64d35
Vigneshwaran-Gurusamy <91051117+Vigneshwaran-Gurusamy@users.noreply.github.com>
2022-02-22 11:18:01 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,4 +5,32 @@
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(20, 10, 15) == TOO_HIGH);
+  REQUIRE(inferBreach(15, 10, 20) == NORMAL);
+}
+
+TEST_CASE("Classify the cooling type and infer the breach") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -4) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 30) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 42) == TOO_HIGH);
+  
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -2) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 42) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 51) == TOO_HIGH);
+  
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -4) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 38) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 42) == TOO_HIGH);
+}
+
+TEST_CASE("Evaluate the breach type and send alerts") {
+  AlertTarget a1 = TO_EMAIL;
+  AlertTarget a2 = TO_CONTROLLER;
+  BatteryCharacter b;
+  b.coolingType = PASSIVE_COOLING;
+  checkAndAlert( a1, b, 40);
+   
+  checkAndAlert( a2, b, -2);
+  
+  checkAndAlert( a1, b, 20);
 }
