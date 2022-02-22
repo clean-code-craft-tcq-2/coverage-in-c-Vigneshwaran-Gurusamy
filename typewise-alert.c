15c9f8dd05c1f425bb9fa5845fac6a3bfcd89a70
Vigneshwaran-Gurusamy <91051117+Vigneshwaran-Gurusamy@users.noreply.github.com>
2022-02-22 12:15:43 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -9,36 +9,43 @@ char alertMsgForEmail[NUMBER_OF_BREACH_TYPES][100] = {"Hi, the temperature is No
                                                    "Hi, the temperature is too High\n"};
 void (*AlertTargetfp[NUMBER_OF_ALERT_TARGETS])(BreachType) = {sendToController, sendToEmail};
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) 
+{
+  if(value < lowerLimit)
+  {
     return TOO_LOW;
   }
-  if(value > upperLimit) {
+  else if(value > upperLimit)
+  {
     return TOO_HIGH;
   }
-  return NORMAL;
+  else
+  {
+    return NORMAL;
+  }
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
+{
   int lowerLimit = limits[coolingType][LOWERLIMIT_IDX];
   int upperLimit = limits[coolingType][UPPERLIMIT_IDX];
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
-void checkAndAlert(
-    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-  
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) 
+{
     BreachType breachType = classifyTemperatureBreach( batteryChar.coolingType, temperatureInC);
     AlertTargetfp[alertTarget](breachType);
 }
 
-void printRecepientAndAlertMsgForEmail(const char* receiver, BreachType breachType){
+void printRecepientAndAlertMsgForEmail(const char* receiver, BreachType breachType)
+{
   printf("To: %s\n", receiver);
   printf("%s", alertMsgForEmail[breachType]);
 }
 
-void sendToController(BreachType breachType) {
+void sendToController(BreachType breachType) 
+{
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
 }
